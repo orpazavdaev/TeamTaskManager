2fbe1aa9e08352e5f419e829b6f4f1cca4993ed8
fernando joey <frenandojoey@gmail.com>
2025-11-10 18:47:48 +0200

    .

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
@@ -18,16 +18,39 @@ export class HeaderComponent {
 
   user = this.authService.user;
   isDarkMode = this.themeService.isDarkMode;
+  showUserMenu = signal(false);
 
   toggleTheme(): void {
     this.themeService.toggleTheme();
   }
 
+  toggleUserMenu(): void {
+    this.showUserMenu.update((value) => !value);
+  }
+
+  closeUserMenu(): void {
+    this.showUserMenu.set(false);
+  }
+
+  navigateToProfile(): void {
+    this.closeUserMenu();
+    this.router.navigate(['/profile']);
+  }
+
   logout(): void {
+    this.closeUserMenu();
     this.authService.logout();
   }
 
   navigateHome(): void {
     this.router.navigate(['/projects']);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.user-menu-container')) {
+      this.closeUserMenu();
+    }
   }
 }
